2eb23d724dcc05504acb2f81d03d53bf1c39d106
Bryan Green <bryangreen82@gmail.com>
2022-12-07 16:39:59 -0600

    add steps for setting username and password and logging in

--- features/step_definitions/test_steps.rb
+++ features/step_definitions/test_steps.rb
@@ -8,3 +8,15 @@ Then(/^the Log In fields are correctly displayed$/) do
   expect(exists { LoginPage.password } ).to be true
   expect(exists { LoginPage.phone_or_email } ).to be true
 end
+
+Then(/^I set the Username field to (.*)$/) do |user_name|
+  LoginPage.phone_or_email.send_keys(user_name)
+end
+
+And(/^I set the Password field to (.*)$/) do |password|
+  LoginPage.password.send_keys(password)
+end
+
+And(/^I tap on the Log In button$/) do
+  LoginPage.login_button.click
+end
--- features/testing.feature
+++ features/testing.feature
@@ -3,3 +3,9 @@ Feature: Facebook Log In page
   Scenario: The log in fields are correctly displayed
     Given I am on the Facebook Log In page
     Then the Log In fields are correctly displayed
+    
+  Scenario: Log into Facebook
+    Given I am on the Facebook Log In page
+    And I set the Username field to bg.automation82@gmail.com
+    And I set the Password field to 1kingGizzard1
+    And
