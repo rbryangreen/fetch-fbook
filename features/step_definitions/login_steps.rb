9b8018d47e9d62958260ce8540fbe9026e412093
Bryan Green <bryangreen82@gmail.com>
2022-12-09 11:53:39 -0600

    some cleanup

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -1,6 +1,6 @@
 Given(/^I am on the Facebook Log In page$/) do
   $driver.wait_true(timeout: 60) { exists { LoginPage.create_new_facebook_account } }
-  expect( exists { LoginPage.create_new_facebook_account } ).to be true
+  expect(exists { LoginPage.create_new_facebook_account } ).to be true
 end
 
 Then(/^the Log In fields are correctly displayed$/) do
