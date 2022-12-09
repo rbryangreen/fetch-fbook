b40f8581d7bbd503b95dc255410f4d1b737f4e24
Bryan Green <bryangreen82@gmail.com>
2022-12-09 09:05:21 -0600

    added lots of commentary explaining various POs

--- features/pages/search_page.rb
+++ features/pages/search_page.rb
@@ -1,13 +1,16 @@
 class SearchPage
 
+  # The 'All' header above the search results
   def self.all_search_results
     $driver.find_element(accessibility_id: 'All search results')
   end
 
+  # The 'Pages' header above the search results (filters search results to Pages only)
   def self.pages_search_results
     $driver.find_element(accessibility_id: 'Pages search results')
   end
 
+  # Use this to enter a value into the search field
   # @param search_term [String] The term you want to search for
   # @param enter [Boolean] Use true if you wish to tap the enter key after the search
   def self.search(search_term)
@@ -16,6 +19,7 @@ class SearchPage
     $driver.action.send_keys(search_term).send_keys(:enter).perform
   end
 
+  # The text entry field for searching
   def self.search_field
     $driver.xpath('//android.widget.EditText')
   end
